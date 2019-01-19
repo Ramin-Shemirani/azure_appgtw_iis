4e2c912a9479ae9292c1f9767765851f7658d183
Ramin-Shemirani <shemirani@conmatix.de>
2019-01-19 19:26:13 +0100

    added audio
    derived from original for testing
    added a new folder named audio

--- install_nginx.sh
+++ install_nginx.sh
@@ -5,3 +5,5 @@ sudo mkdir -p /var/www/html/images
 echo "Images: " $(hostname) | sudo tee /var/www/html/images/test.html
 sudo mkdir -p /var/www/html/video
 echo "Video: " $(hostname) | sudo tee /var/www/html/video/test.html
+sudo mkdir -p /var/www/html/audio
+echo "Video: " $(hostname) | sudo tee /var/www/html/audio/test.html
